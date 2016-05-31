312c5bc443f0b8e75ec0c1fceb258852c729fae1
leegoway <leego.sir@gmail.com>
2016-05-31 13:37:30 +0800

    返回值json格式化

--- JsonResponse/JsonViewer.php
+++ JsonResponse/JsonViewer.php
@@ -3,31 +3,33 @@
 class JsonViewer {
 	
 	private $code = 200;
-	private $data = null;
-	private $msg = null;
+    private $data = null;
+    private $msg = null;
 
-	public $contentType = 'application/json';
-	public $encodingOptions = 0;
+    public $encodingOptions = 0;
 
-	public function render($data, $status = 200, $msg = '') {
-        $app = \Slim\Slim::getInstance();
+    private $_response;
+
+    public function __construct($response) {
+        $this->_response = $response;
+    }
+
+    public function render($data, $status = 200, $msg = '') {
+        
         $status = intval($status);
         $response = ['code' => 200, 'data' => null, 'msg' => ''];
         $response['code'] = $status;
         if(200 == $status){
-        	$response['data'] = $data;
+            $response['data'] = $data;
         }else{
-        	$response['msg'] = $msg;
+            $response['msg'] = $msg;
         }
-		
-        $app->response()->status(200);
-        $app->response()->header('Content-Type', $this->contentType);
-        $jsonp_callback = $app->request->get('callback', null);
-        if($jsonp_callback !== null){
-            $app->response()->body($jsonp_callback.'('.json_encode($response, $this->encodingOptions).')');
-        } else {
-            $app->response()->body(json_encode($response, $this->encodingOptions));
-        }
-        $app->stop();
+        $this->_response->withJson($response);
+    }
+
+    public function renderException($status = 0, $msg = '') {
+        $status = intval($status);
+        $response = ['code' => $status, 'data' => null, 'msg' => $msg];
+        $this->_response->withJson($response);
     }
 }
